362b873965b6bb38abb2d90e6d19dc6d71861a2b
ipriyam26 <ipriyam26@gmail.com>
2022-08-19 20:43:26 +0530

    splash screen added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:waste_not/screens/food.dart';
+import 'package:waste_not/screens/splash.dart';
 
 void main() {
   runApp(const MyApp());
@@ -25,7 +26,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: Food(),
+      home: const SplashScreen(),
     );
   }
 }
